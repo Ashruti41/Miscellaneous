91dfc0fb7caee8f31ac707f80f0e88aca6a6040c
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-11 22:39:51 +0530

    Create e2.c

---
+++ Excercise/e2.c
@@ -0,0 +1,16 @@
+//Q2
+
+
+#include<stdio.h>
+enum day{sun=1, mon,tue,wed};
+int main()
+{
+    enum day d1;
+    printf("%d\t",mon);
+    d1=mon+2;
+    printf("%d\t",d1);
+}
+
+/*output:
+2  4
+*/
