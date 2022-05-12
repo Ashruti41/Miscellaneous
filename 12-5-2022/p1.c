263334649deb6285be936428dac471d66cb3a492
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-12 16:58:13 +0530

    Create p1.c

---
+++ 12-5-2022/p1.c
@@ -0,0 +1,31 @@
+//find out the sum of integers
+
+#include<stdio.h>
+#include<stdarg.h>
+int sum(int , ...);
+int main()
+{
+    printf("Total = %d\n",sum(2,99,68));                //here 2 is a number and totoal is calculate with 99+68=167
+    printf("Total = %d\n",sum(3,11,79,32));
+    printf("Total = %d\n",sum(5,23,34,45,56,78));
+}
+int sum(int num, ...)
+{
+    int i;
+    va_list ap;
+    int arg, total=0;
+    va_start(ap,num);
+    for(i=0; i<num; i++)
+    {
+        arg=va_arg(ap,int);
+        printf("%d ",arg);
+        total+=arg;
+    }
+    va_end(ap);
+    return total;
+}
+/*output:
+99 68 Total = 167
+11 79 32 Total = 122
+23 34 45 56 78 Total = 236
+*/
